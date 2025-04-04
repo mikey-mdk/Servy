f4bcf44e0b3766a2f942e410bb1eda592533d2c5
Michael Koch <michael.david.mdk@gmail.com>
2025-04-03 18:19:31 -0600

    App level supervisors

--- lib/servy.ex
+++ lib/servy.ex
@@ -1,6 +1,8 @@
 defmodule Servy do
+  use Application
 
-  def hello(name) do
-    "Howdy #{name}!"
+  def start(_type, _args) do
+    IO.puts "Starting The Application..."
+    {:ok, _sup_pid} = Servy.Supervisor.start_link()
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Servy.MixProject do
   def project do
     [
       app: :servy,
+      description: "A humble HTTP server",
       version: "0.1.0",
       elixir: "~> 1.18.3",
       start_permanent: Mix.env() == :dev,
@@ -14,7 +15,9 @@ defmodule Servy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools, :wx, :observer]
+      extra_applications: [:logger, :runtime_tools, :wx, :observer],
+      mod: {Servy, []},
+      env: [port: 3000]
     ]
   end
 
